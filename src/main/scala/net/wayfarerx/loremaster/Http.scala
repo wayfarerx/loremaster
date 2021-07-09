0bf76e8bb439eb8cc2c0200dfd7db31879ee0929
wayfarerx <x@wayfarerx.net>
2021-07-09 01:58:17 -0600

    Working on http.

--- src/main/scala/net/wayfarerx/loremaster/Http.scala
+++ src/main/scala/net/wayfarerx/loremaster/Http.scala
@@ -24,45 +24,48 @@ trait Http {
   import Http.State
 
   /**
-   * Downloads the content at the specified URI if it exists.
+   * Downloads the state of the specified URI.
    *
-   * @param uri The URI of the content to download.
-   * @param ifModifiedAfter The optional constraint on when the content was last modified.
-   * @return The content at the specified URI URI if it exists.
+   * @param uri The URI of the state to download.
+   * @param ifModifiedAfter The optional constraint on when the state was last modified.
+   * @return The state of the specified URI.
    */
   def get(uri: URI, ifModifiedAfter: Option[Instant] = None): Task[State]
 
   /**
-   * Downloads the content at the specified URI if it exists.
+   * Downloads the state of the specified URI.
    *
-   * @param uri The URI of the content to download.
-   * @param ifModifiedAfter The constraint on when the content was last modified.
-   * @return The content at the specified URI URI if it exists.
+   * @param uri The URI of the state to download.
+   * @param ifModifiedAfter The constraint on when the state was last modified.
+   * @return The state of the specified URI.
    */
   inline final def get(uri: URI, ifModifiedAfter: Instant): Task[State] = get(uri, Some(ifModifiedAfter))
 
 }
 
+/**
+ * Definitions associated with http services.
+ */
 object Http:
 
   import zio.{Has, RIO}
 
   /**
-   * Downloads the content at the specified URI if it exists.
+   * Downloads the state of the specified URI.
    *
-   * @param uri The URI of the content to download.
-   * @param ifModifiedAfter The optional constraint on when the content was last modified.
-   * @return The content at the specified URI URI if it exists.
+   * @param uri The URI of the state to download.
+   * @param ifModifiedAfter The optional constraint on when the state was last modified.
+   * @return The state of the specified URI.
    */
   inline def get(uri: URI, ifModifiedAfter: Option[Instant] = None): RIO[Has[Http], State] =
     RIO.service flatMap (_.get(uri, ifModifiedAfter))
 
   /**
-   * Downloads the content at the specified URI if it exists.
+   * Downloads the state of the specified URI.
    *
-   * @param uri The URI of the content to download.
-   * @param ifModifiedAfter The constraint on when the content was last modified.
-   * @return The content at the specified URI URI if it exists.
+   * @param uri The URI of the state to download.
+   * @param ifModifiedAfter The constraint on when the state was last modified.
+   * @return The state of the specified URI.
    */
   inline def get(uri: URI, ifModifiedAfter: Instant): RIO[Has[Http], State] =
     RIO.service flatMap (_.get(uri, ifModifiedAfter))
@@ -75,4 +78,4 @@ object Http:
 
     case object NotModified extends State
 
-    case class Latest(content: String) extends State
+    case class Latest(state: String) extends State
